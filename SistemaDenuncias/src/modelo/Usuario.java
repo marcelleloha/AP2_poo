731d1f51a805eb618e0470886808ce1d587591c7
Eduardo Peruzzo <eduardo.peruzzo05@gmail.com>
2025-06-02 18:26:03 -0300

    estrutura de Usuario

--- SistemaDenuncias/src/modelo/Usuario.java
+++ SistemaDenuncias/src/modelo/Usuario.java
@@ -3,12 +3,12 @@ package modelo;
 import java.util.ArrayList;
 
 public class Usuario {
-    private int idUsuario;
+    private final int idUsuario;
     private String nome;
     private String email;
     private String senha;
     private boolean ehAdm;
-    private ArrayList<Denuncia> denuncias;
+    private ArrayList<Denuncia> denuncias = new ArrayList<Denuncia>();
 
     private static int id = 0;
 
@@ -20,15 +20,49 @@ public class Usuario {
         this.idUsuario = ++id;
     }
 
-//- alterarSenha(novaSenha)
-//- criarDenuncia(categoria, desc, Localizacao, Midia)
-//- comentar(conteudo, Denuncia)
-//- editarComentario(Comentario)
-//- removerComentario(Comentario)
-//- removerDenuncia(Denuncia)
-//- editarDenuncia(Denuncia, dado, novoDado)
-//- votar(voto, Denuncia ou Comentario)
-//- removerVoto(Denuncia ou Comentario)
+// TODO alterarSenha(novaSenha) - perguntar como fazer a verificação, se precisa de segurança rígida ou não
 
-    
+    public boolean criarDenuncia(Categorias categoria, String desc, Localizacao local, Midia midia) {
+        // verificar se já existe outra denúncia igual no sistema (mesma categoria e local) antes de criar. se já houver uma igual, retorna false
+
+        // Denuncia novaDenuncia = new Denuncia(categoria, desc, local, midia);
+    }
+
+    public boolean editarDenuncia(Denuncia denuncia, String dado, Object novoDado) {
+        // edita uma denúncia deste usuário, mudando um de seus atributos (dado) para o valor de novoDado
+
+        // fazer uma verificação com 'dado' para descobrir a qual atributo da denúncia ele se refere
+    }
+
+    public boolean comentar(String conteudo, Denuncia denuncia) {
+        // cria um objeto Comentario e o associa a uma Denuncia
+    }
+
+    public boolean editarComentario(Comentario c, String novoConteudo) {
+        // substitui o conteúdo de um comentário que pertença a esse usuário
+    }
+
+    public boolean removerComentario(Comentario c) {
+        // apaga um comentário que pertença a esse usuário
+    }
+
+    public boolean removerDenuncia(Denuncia d) {
+        // caso o usuário seja um administrador, pode remover qualquer comentário, caso não, apenas os comentários deste usuário podem ser deletados
+    }
+
+    public boolean votar(int voto, Denuncia d) {
+        // adiciona um voto (valor inteiro entre 1 e 10) a uma denuncia, cada usuário só pode votar uma vez em cada denúncia
+    }
+
+    public boolean votar(int voto, Comentario c) {
+        // adiciona um voto (+1) a um comentário, cada usuário só pode votar uma vez em cada comentário
+    }
+
+    public boolean removerVoto(Denuncia d) {
+        // remove o voto deste usuário de uma denúncia
+    }
+
+    public boolean removerVoto(Comentario c) {
+        // remove o voto deste usuário de um comentário
+    }
 }
